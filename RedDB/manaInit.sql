7b51d4c1fa2afc1cf3fde3a9437e758c841905cc
Daniel Arad <sisitrs2@gmail.com>
2022-03-20 13:30:48 +0200

    Added pointer to files in server.

--- RedDB/manaInit.sql
+++ RedDB/manaInit.sql
@@ -1,8 +1,5 @@
 -- Redeye users:
-INSERT INTO redeye_users(username,password) VALUES("Elad", "123");
-INSERT INTO redeye_users(username,password) VALUES("Daniel", "123");
-INSERT INTO redeye_users(username,password) VALUES("Danino", "123");
-INSERT INTO redeye_users(username,password) VALUES("Yoch", "123");
+INSERT INTO redeye_users(username,password) VALUES("Admin", "123");
 
 -- Projects:
 INSERT INTO projects(name, filename) VALUES ("ExampleDB", "example.db");
