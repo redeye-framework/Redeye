f9b3d45a7cf0b893f8890b77806f96c8e291bbbe
elad_pticha <ldpth1@gmail.com>
2023-04-27 22:47:22 +0300

    Updated auth to support Read or Read/Write permissions. Add logs api.

--- api.py
+++ api.py
@@ -12,25 +12,35 @@ api_route = Blueprint('api', __name__)
 PROJECTS = r"RedDB/Projects/{}"
 
 
-def return_json(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        token = request.headers.get('Token')
-        token_details = get_hashed_token_details(sha256(token.encode()).hexdigest())
-        if not token_details:
-            return jsonify({ "status" : 401 })
+def authentication(access_level, resource):
+
+    def outer(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            token = request.headers.get('Token')
+            token_details = get_hashed_token_details(sha256(token.encode()).hexdigest())
+            if not token_details:
+                return jsonify({ "status" : 401 })
+            
+            permissions = json.loads(token_details[0][3])
+            token_access_level = permissions.get('access_level')
+            token_auth = permissions.get('auth')
+
+            if access_level == token_access_level and \
+                token_auth.get(resource):
+                response = f(token_details[0], request.args.to_dict())
+                return response
+
+            else:
+                return jsonify({"status": 401})
         
-        response = f(token_details[0], request.args.to_dict())
-        return response
-    
-    return inner
+        return inner
+    return outer
+
 
 @api_route.route('/api/servers',methods=['GET'])
-@return_json
+@authentication('READ', 'servers')
 def api_get_servers(token_data, args):
-    permissions = json.loads(token_data[3])
-    if not permissions.get('servers'):
-        return jsonify({"status" : 403})
     
     db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
     servers = jdb.servers_info(db_name, args)
@@ -39,12 +49,8 @@ def api_get_servers(token_data, args):
 
 
 @api_route.route('/api/exploits',methods=['GET'])
-@return_json
+@authentication('READ', 'exploits')
 def api_get_exploits(token_data, args):
-    permissions = json.loads(token_data[3])
-    if not permissions.get('exploits'):
-        return jsonify({"status" : 403})
-    
 
     db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
     exploits = jdb.exploits_info(db_name, args)
@@ -53,12 +59,8 @@ def api_get_exploits(token_data, args):
 
 
 @api_route.route('/api/files',methods=['GET'])
-@return_json
+@authentication('READ', 'files')
 def api_get_files(token_data, args):
-    permissions = json.loads(token_data[3])
-    if not permissions.get('files'):
-        return jsonify({"status" : 403})
-    
 
     db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
     files = jdb.files_info(db_name, args)
@@ -67,13 +69,20 @@ def api_get_files(token_data, args):
 
 
 @api_route.route('/api/users',methods=['GET'])
-@return_json
+@authentication('READ', 'users')
 def api_get_users(token_data, args):
-    permissions = json.loads(token_data[3])
-    if not permissions.get('users'):
-        return jsonify({"status" : 403})
     
     db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
     users = jdb.users_info(db_name, args)
+
+    return jsonify(users)
+
+
+@api_route.route('/api/logs',methods=['GET'])
+@authentication('READ', 'logs')
+def api_get_logs(token_data, args):
+    
+    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+    users = jdb.logs_info(db_name, args)
 
     return jsonify(users)
