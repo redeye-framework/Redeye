8b370bbc23646428dc9d0a87be4f19bc8c3219ba
elad_pticha <ldpth1@gmail.com>
2023-04-28 12:26:11 +0300

    Removed duplicate code.

--- api.py
+++ api.py
@@ -31,11 +31,12 @@ def authentication(access_level, resource):
 
             if access_level <= token_access_level and \
                 token_auth.get(resource):
-                response = f(token_details[0], request.args.to_dict())
+                db_name = PROJECTS.format(db.get_project_filename_by_id(token_details[0][6]))
+                response = f(db_name, request.args.to_dict())
                 return response
 
             else:
-                return jsonify({"status": 401})
+                return jsonify({"status": 403})
         
         return inner
     return outer
@@ -43,9 +44,7 @@ def authentication(access_level, resource):
 
 @api_route.route('/api/servers',methods=['GET'])
 @authentication(READ, 'servers')
-def api_get_servers(token_data, args):
-    
-    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+def api_get_servers(db_name, args):
     servers = jdb.servers_info(db_name, args)
 
     return jsonify(servers)
@@ -53,9 +52,7 @@ def api_get_servers(token_data, args):
 
 @api_route.route('/api/exploits',methods=['GET'])
 @authentication(READ, 'exploits')
-def api_get_exploits(token_data, args):
-
-    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+def api_get_exploits(db_name, args):
     exploits = jdb.exploits_info(db_name, args)
 
     return jsonify(exploits)
@@ -63,9 +60,7 @@ def api_get_exploits(token_data, args):
 
 @api_route.route('/api/files',methods=['GET'])
 @authentication(READ, 'files')
-def api_get_files(token_data, args):
-
-    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+def api_get_files(db_name, args):
     files = jdb.files_info(db_name, args)
 
     return jsonify(files)
@@ -73,9 +68,7 @@ def api_get_files(token_data, args):
 
 @api_route.route('/api/users',methods=['GET'])
 @authentication(READ, 'users')
-def api_get_users(token_data, args):
-    
-    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+def api_get_users(db_name, args):
     users = jdb.users_info(db_name, args)
 
     return jsonify(users)
@@ -83,9 +76,7 @@ def api_get_users(token_data, args):
 
 @api_route.route('/api/logs',methods=['GET'])
 @authentication(READ, 'logs')
-def api_get_logs(token_data, args):
-    
-    db_name = PROJECTS.format(db.get_project_filename_by_id(token_data[6]))
+def api_get_logs(db_name, args):
     users = jdb.logs_info(db_name, args)
 
     return jsonify(users)
